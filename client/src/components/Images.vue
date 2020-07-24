653071e5d1bf72071962cd869072ab3cb2351004
Ivan Alles <ivan.alles@gmail.com>
2020-07-24 14:32:45 +0200

    Create server-backend.js

--- client/src/components/Images.vue
+++ client/src/components/Images.vue
@@ -27,20 +27,7 @@
 </template>
 
 <script>
-import axios from 'axios';
-
-class Backend {
-    async getImages() {
-      const path = 'http://localhost:5000/images';
-      let result = await axios.get(path);
-      return result.data.images;
-    }
-
-    async learn(likes) {
-      const path = 'http://localhost:5000/learn';
-      await axios.post(path, likes);
-    }
-}
+import { Backend } from '@/server-backend.js'
 
 export default {
   data() {
