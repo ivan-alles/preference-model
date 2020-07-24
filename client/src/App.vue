6412b508de7e5baf91cc6ce9bd2fe32f4aaca199
Ivan Alles <ivan.alles@gmail.com>
2020-07-24 17:47:35 +0200

    Whitespace

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,7 +5,7 @@
       <router-link to="/about">About</router-link>
     </div>
     <keep-alive>
-        <router-view></router-view>
+      <router-view></router-view>
     </keep-alive>
   </div>
 </template>
