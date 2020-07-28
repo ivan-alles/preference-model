271ec7ef1942a757e712c014caa70b39f8e5a3ed
Ivan Alles <ivan.alles@gmail.com>
2020-07-28 16:49:22 +0200

    Add bootstrap.

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import Home from '../views/Home.vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 Vue.use(VueRouter)
 
   const routes = [
