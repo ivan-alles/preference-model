042200f2869cdbded39b23d92ab5d67a83a508dc
Ivan Alles <ivan.alles@gmail.com>
2020-08-04 18:22:20 +0200

    Test for scaledDirichlet()

--- client/tests/unit/client-engine.spec.js
+++ client/tests/unit/client-engine.spec.js
@@ -18,4 +18,42 @@ test('scaledDirichlet() shall work with various random parameters', () => {
 
   expect(x.shape).toStrictEqual([n, k]);
 
+  expectTensorsClose(x.sum(1), tf.ones([n]));
+  
+  expectTensorsEqual(tf.greaterEqual(x, (1 - scale) / k), tf.ones([n, k]));
+  expectTensorsEqual(tf.lessEqual(x, (scale * (k-1) + 1) / k), tf.ones([n, k]));
+  expectTensorsClose(x.mean(0), tf.fill([k], 1 / k), 0.1);
+  const stdExp = scale * Math.sqrt((k-1) / k**2 / (k * a + 1));
+  expectTensorsClose(std(x, 0), tf.fill([k], stdExp), 0.1)
 });
+
+/**
+ * Check if tensors are close. Is needed as tf.test_util.expectArraysClose()
+ * always succeedes with tensor arguments.
+ *
+ */
+function expectTensorsClose(actual, expected, epsilon=null) {
+  tf.test_util.expectArraysClose(
+    actual.arraySync(),
+    expected.arraySync(),
+    epsilon);
+}
+
+/**
+ * Check if tensors are close. Is needed as tf.test_util.expectArraysClose()
+ * always succeedes with tensor arguments.
+ *
+ */
+function expectTensorsEqual(actual, expected) {
+  tf.test_util.expectArraysEqual(
+    actual.arraySync(),
+    expected.arraySync());
+}
+
+function std(x, axis=null) {
+  const n = x.shape[axis];
+  const m = tf.mean(x, axis)
+  const xc = tf.sub(x, m)
+  const x2 = tf.square(xc)
+  return tf.sqrt(tf.div(tf.sum(x2, axis), n));
+}
