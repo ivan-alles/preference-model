2d5b22f0db017e244fd34c2e52eafeb56dac2680
Ivan Alles <ivan.alles@gmail.com>
2020-08-10 12:22:43 +0200

    Deploy model on gh-pages

--- client/deploy.bat
+++ client/deploy.bat
@@ -5,9 +5,6 @@ call yarn build
 
 pushd dist
 
-:: The models are too large, delete them to avoid pushing to GitHub.
-for /d /r %%i in (*.tfjs) do @rmdir /S /Q "%%i"
-
 git init
 git add -A
 git commit -m 'deploy'
