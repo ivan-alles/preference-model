bdbd9ea1d008e4c3f55f2e1db09ca8b6c438ff13
Ivan Alles <ivan.alles@gmail.com>
2020-08-10 08:57:14 +0200

    About page

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
+    <section>
+    <h2>Description</h2>
+    This <a href="https://github.com/ivan-alles/preference-model">app</a> demonstrates learning your preferences through machine learning. 
+    </section>
+
+    <section>
+    <h2>Credits</h2>
+    The generator is from the <a href="https://github.com/tkarras/progressive_growing_of_gans">Progressive Growing of GANs for Improved Quality, Stability, and Variation</a> by Tero Karras (NVIDIA), Timo Aila (NVIDIA), Samuli Laine (NVIDIA), Jaakko Lehtinen (NVIDIA and Aalto University).
+    This is a demonstration of learning user preferences by machine learning.
+    </section>
   </div>
 </template>
