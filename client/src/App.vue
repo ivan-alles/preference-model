e1efd1e9009a47342313bd947e3549dbc5fa0db1
Ivan Alles <ivan.alles@gmail.com>
2020-08-11 11:33:39 +0200

    Use navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <b-navbar type="dark" variant="dark">
+      <b-navbar-nav>
+        <b-nav-item to="/">Home</b-nav-item>
+        <b-nav-item to="/about">About</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
     <keep-alive>
       <router-view></router-view>
     </keep-alive>
