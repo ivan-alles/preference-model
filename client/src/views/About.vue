64ff23887961bfcc72268e0885748fe9b91143e2
Ivan Alles <ivan.alles@gmail.com>
2020-08-14 13:02:27 +0200

    How it works

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -3,9 +3,26 @@
     <header>
       <h1>About</h1>
       <p>
-      This <a href="https://github.com/ivan-alles/preference-model">app</a> demonstrates learning your preferences through machine learning. 
+      This <a href="https://github.com/ivan-alles/preference-model">app</a> demonstrates how a neural network learns what you like.
       </p>
     </header>
+    <section>
+      <h2>How it works</h2>
+      <p>
+      At the beginning you will see random pictures, dreamed up by a neural network. This neural network is called generator. 
+      The people on the pictures look real, but they do not exist!
+      </p>
+      <p>
+      When you like some pictures and press <b>Learn</b>, another piece of AI will learn them. The generator starts to create new pictures 
+      sharing common traits of your likes. 
+      </p>
+      <p>
+      You can vary the pictures less or more by changing the <b>Variance</b>.
+      </p>
+      <p>
+      Now you can like the best of the new pictures to create even more beatiful ones. Eventually you can let the AI dream up a person of your dream!
+      </p>
+    </section>
     <section>
       <h2>Credits</h2>
       <p>
