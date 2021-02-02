72b51231e0ef2ec813cda176d7bbf44daafac930
Ivan Alles <ivan.alles@gmail.com>
2021-02-02 13:10:59 +0100

    Do not create dist.

--- convert_to_js/convert.bat
+++ convert_to_js/convert.bat
@@ -1,10 +1,7 @@
 @echo off
 
 rem Convert a TensorFlow model to a TensorFlow.js model.
-rem Convert to dist folder, as the parcel server will search for files there.
 rem Paramters:
 rem %1 path to model
-
-if not exist dist mkdir dist
 
 tensorflowjs_converter %1 %1js --quantize_float16
