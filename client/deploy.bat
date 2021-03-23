50b0020e4608c668948959f540f253ae093e696b
Ivan Alles <ivan.alles@gmail.com>
2021-03-23 16:30:22 +0100

    Remove About.vue

--- client/deploy.bat
+++ client/deploy.bat
@@ -5,9 +5,6 @@ call yarn build
 
 pushd dist
 
-:: This fixes the problem of Github Pages showing ERROR 404 for SPA routes.
-copy index.html about.html
-
 git init
 git add -A
 git commit -m 'deploy'
