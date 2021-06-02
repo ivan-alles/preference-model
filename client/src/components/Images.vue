100b1277e088287986098eb429cbd0a17fe91150
Ivan Alles <ivan.alles@gmail.com>
2021-06-02 16:14:15 +0200

    Update text.

--- client/src/components/Images.vue
+++ client/src/components/Images.vue
@@ -49,32 +49,24 @@
     </template>
     <template v-if="state === stateKind.WELCOME">
       <div class="main-text">
-        <header>
-          <p>
-          This <a href="https://github.com/ivan-alles/preference-model">app</a> demonstrates how a neural network learns what you like.
-          </p>
-        </header>
         <section>
-          <h2>How it works</h2>
-          <p>
-          In the beginning, you will see random pictures, dreamed up by a neural network. This neural network is called a generator. 
-          The people on the images look real, but they do not exist!
-          </p>
-          <p>
-          When you like some pictures, the AI will learn them. The generator starts to create new images 
-          sharing the common traits of your likes. 
-          </p>
-          <p>
-          You can vary the pictures less or more by changing the <b>Variance</b>.
-          </p>
-          <p>
-          Now you can like the best of the new pictures to create even more beautiful ones. Eventually, you can let the AI create the person of your dream!
-          </p>
+          <p>This app demonstrates how computational creativity can work together with you to create beautiful pictures.</p>
+
+          <p>In the beginning, you will see random pictures dreamed up by a neural network. This neural network is called a generator. The people on the images look natural, but they do not exist!
+
+          <p>When you like some pictures, the AI will learn them. Then, the generator starts to create new images sharing the common traits of your likes.</p>
+
+          <p>You can vary the pictures less or more by changing the <b>Variance</b>.</p>
+
+          <p>Now you can select the best of the new pictures to create even more beautiful ones. Eventually, you can let the AI make the person of your dreams!</p>
+
+          <p>See more on <a href="https://github.com/ivan-alles/preference-model">Github</a>.</p>
         </section>
         <section>
           <h2>Credits</h2>
           <p>
-          The generator is from the <a href="https://github.com/tkarras/progressive_growing_of_gans">Progressive Growing of GANs for Improved Quality, Stability, and Variation</a> by Tero Karras (NVIDIA), Timo Aila (NVIDIA), Samuli Laine (NVIDIA), Jaakko Lehtinen (NVIDIA and Aalto University).
+          The generator is from the <a href="https://github.com/tkarras/progressive_growing_of_gans">Progressive Growing of GANs for Improved Quality, Stability, and Variation</a> 
+          by Tero Karras, Timo Aila, Samuli Laine, and Jaakko Lehtinen, licensed under the Creative Commons CC BY-NC 4.0 license.
           </p>
         </section>
       </div>
